0cc1b0e8a8c01b7e6e1318e3bd49d3117b3a78d3
Isaac Grant <waterwolf2.0@gmail.com>
2012-07-25 10:57:15 -0600

    new parse method

--- ArgParser.py
+++ ArgParser.py
@@ -2,7 +2,7 @@
 
 import os, sys
 
-USAGE = """ser.py [search root] [thing to find] [mode (file | folder | ext | any)] [verbose (true | false) --loose
+USAGE = """ser.py [search root] [thing to find] [mode (--file | --folder | --ext)] [--verbose] [--loose]
 examples:
 ser.py C:\\ foo.txt
 ser.py C:\\ foobar folder
@@ -10,7 +10,11 @@ ser.py C:\\ bar ext true"""
 
 class Parser:
     LOOSE_FLAGS = ['--loose', '-l', '--l']
+    MODE_FLAGS = ['--file', '--folder', '--ext']
+    VERBOSE_FLAGS = ['--verbose', '-v']
+    MODES = ['file', 'folder', 'ext']
     _INVALID_ROOT = 'Invalid search root, defaulting to {!s}'
+    
     def __init__(self, args):
         self.args = args
     
@@ -23,39 +27,26 @@ class Parser:
     def validate_args(self):
         if len(self.args) > 1:
             if not os.path.exists(self.args[0]) or not os.path.isdir(self.args[0]):
-                if 'linux' in sys.platform:
-                    self.args[0] = os.path.sep
-                elif 'win' in sys.platform:
-                    self.args[0] = os.path.join('C:', os.path.sep)
-                print(self._INVALID_ROOT.format(self.args[0]))
-            if len(self.args) > 2:
-                if not self.args[2] == 'file' and not self.args[2] == 'folder' and not self.args[2] == 'ext':
-                    self.args[2] == 'any'
-                if len(self.args) >= 4:
-                    if self.args[3] == 'true' or self.args[3] == 't':
-                        self.args[3] = True
-                    elif self.args[3] == 'false' or self.args[3] == 'f':
-                        self.args[3] = False
-                    else:
-                        self.args[3] = False
-                    if len(self.args) == 5:
-                        if self.args[4] in self.LOOSE_FLAGS:
-                            self.args[4] = self.LOOSE_FLAGS[0]
-                else:
-                    self.args.append(False)
-            elif len(self.args) == 2:
-                self.args.append('any')
-                self.args.append(False)
+                print(USAGE)
+                sys.exit(2) # error code 2 for bad root directory
         else:
             print(USAGE)
             sys.exit(1) # error code 1 is too few args
-    
+        
     def parse(self):
-        self.validate_args()
         args = {}
         args['root'] = self.args[0]
         args['target'] = self.args[1]
-        args['mode'] = self.args[2]
-        args['verbose'] = self.args[3]
-        args['loose'] = True if self.args[4] == self.LOOSE_FLAGS[0] else False
+        args['mode'] = 'any'
+        for MODE_FLAG in self.MODE_FLAGS:
+            if MODE_FLAG in self.args:
+                args['mode'] = self.MODES[self.MODE_FLAGS.index(MODE_FLAG)]
+        args['verbose'] = False
+        for VERBOSE_FLAG in self.VERBOSE_FLAGS:
+            if VERBOSE_FLAG in self.args:
+                args['verbose'] = True
+        args['loose'] = False
+        for LOOSE_FLAG in self.LOOSE_FLAGS:
+            if LOOSE_FLAG in self.args and not args['mode'] == 'ext':
+                args['loose'] = True
         return args
