5751037416b2b5f228085485684bb4ce083189b6
Isaac Grant <waterwolf2.0@gmail.com>
2012-07-25 10:13:54 -0600

    added option for loose search

--- Search.py
+++ Search.py
@@ -6,85 +6,85 @@ class Search:
     SEARCH_HALTED = 'Stopped due to keyboard interrupt'
     win_priorities = [os.path.join('C:', 'Windows', 'System32'), os.path.join('C:', 'Users')]
     nix_priorities = [os.path.join(os.path.sep, 'etc'), os.path.join(os.path.sep, 'media'),
-            os.path.join(os.path.sep, 'home')]
+	    os.path.join(os.path.sep, 'home')]
 
     _NO_PRIORITIES = False
 
     def __init__(self, root, target, mode, verbose):
-        """Init an instance with the root of your search, along with the file to be searched for"""
-        self.root = root
-        self.target = target
-        self.mode = mode
-        self.verbose = verbose
-        self.results = []
-        self.priorities = self.win_priorities if 'win' in sys.platform else self.nix_priorities
+	"""Init an instance with the root of your search, along with the file to be searched for"""
+	self.root = root
+	self.target = target
+	self.mode = mode
+	self.verbose = verbose
+	self.results = []
+	self.priorities = self.win_priorities if 'win' in sys.platform else self.nix_priorities
     def __str__(self):
-        return 'search object for finding %s starting at %s' % (self.target, self.root)
+	return 'search object for finding %s starting at %s' % (self.target, self.root)
     def _notify(self, name, folder):
-        self.results.append(os.path.join(folder, name))
-        if self.verbose:
-            print('Found %s in %s!' % (name, folder))
+	self.results.append(os.path.join(folder, name))
+	if self.verbose:
+	    print('Found %s in %s!' % (name, folder))
     def _search(self, root):
-        for folder, sub_folders, files in os.walk(root, followlinks=True):
-            if self.mode == 'any':
-                if self.target in sub_folders or self.target in files:
-                    self._notify(self.target, folder)
-            elif self.mode == 'file':
-                if self.target in files:
-                    self._notify(self.target, folder)
-            elif self.mode == 'folder':
-                if self.target in sub_folders:
-                    self._notify(self.target, folder)
+	for folder, sub_folders, files in os.walk(root, followlinks=True):
+	    if self.mode == 'any':
+		if self.target in sub_folders or self.target in files:
+		    self._notify(self.target, folder)
+	    elif self.mode == 'file':
+		if self.target in files:
+		    self._notify(self.target, folder)
+	    elif self.mode == 'folder':
+		if self.target in sub_folders:
+		    self._notify(self.target, folder)
     def search(self):
-        try:
-            self._search(self.root)
-            if not self._NO_PRIORITIES:
-                for priority in self.priorities:
-                    self._search(priority)
-        except KeyboardInterrupt:
-            print(self.SEARCH_HALTED)
+	try:
+	    self._search(self.root)
+	    if not self._NO_PRIORITIES:
+		for priority in self.priorities:
+		    self._search(priority)
+	except KeyboardInterrupt:
+	    print(self.SEARCH_HALTED)
 
 class LooseSearch(Search):
     _NO_PRIORITIES = True
     
     def _search(self, root):
-        for folder, sub_folders, files in os.walk(root, followlinks=True):
-            if self.mode == 'any':
-                if self.target in sub_folders or self.target in files:
-                    self._notify(self.target, folder)
-                for sub_folder in sub_folders:
-                    if self.target in sub_folder:
-                        self._notify(sub_folder, folder)
-                for f in files:
-                    if self.target in f:
-                        self._notify(f, folder)
-            elif self.mode == 'file':
-                if self.target in files:
-                    self._notify(self.target, folder)
-                for f in files:
-                    if self.target in f:
-                        self._notify(f, folder)
-            elif self.mode == 'folder':
-                if self.target in sub_folders:
-                    self._notify(self.target, folder)
-                for sub_folder in sub_folders:
-                    if self.target in sub_folder:
-                        self._notify(sub_folder, folder)
+	for folder, sub_folders, files in os.walk(root, followlinks=True):
+	    if self.mode == 'any':
+		if self.target in sub_folders or self.target in files:
+		    self._notify(self.target, folder)
+		for sub_folder in sub_folders:
+		    if self.target in sub_folder:
+			self._notify(sub_folder, folder)
+		for f in files:
+		    if self.target in f:
+			self._notify(f, folder)
+	    elif self.mode == 'file':
+		if self.target in files:
+		    self._notify(self.target, folder)
+		for f in files:
+		    if self.target in f:
+			self._notify(f, folder)
+	    elif self.mode == 'folder':
+		if self.target in sub_folders:
+		    self._notify(self.target, folder)
+		for sub_folder in sub_folders:
+		    if self.target in sub_folder:
+			self._notify(sub_folder, folder)
 
 class ExtSearch(Search):
     _NO_PRIORITIES = True
     
     def __init__(self, root, target, verbose):
-        self.root = root
-        self.target = target
-        self.verbose = verbose
-        self.results = []
-        
+	self.root = root
+	self.target = target
+	self.verbose = verbose
+	self.results = []
+	
     def _search(self, root):
-        for folder, sub_folders, files in os.walk(root, followlinks=True):
-            for f in files:
-                try:
-                    if f.split(os.path.extsep)[1] == self.target:
-                        self._notify(f, folder)
-                except IndexError:
-                    pass
+	for folder, sub_folders, files in os.walk(root, followlinks=True):
+	    for f in files:
+		try:
+		    if f.split(os.path.extsep)[1] == self.target:
+			self._notify(f, folder)
+		except IndexError:
+		    pass
--- ser.py
+++ ser.py
@@ -10,30 +10,35 @@ POSITIVE_CHOICES = ['yes', 'y', 'Y']
 NEGATIVE_CHOICES = ['no', 'n', 'N']
 
 def publish_results(results):
-	print(SEARCH_COMPLETE.format(len(results)))
-	for result in results:
-		print(result)
+    print(SEARCH_COMPLETE.format(len(results)))
+    for result in results:
+        print(result)
 
 def main(args):
-	if args['mode'] == 'ext':
-		from Search import ExtSearch as ext_search
-		print(INFO_STOP)
-		search = ext_search(args['root'], args['target'], args['verbose'])
-	else:
-		from Search import Search as reg_search
-		search = reg_search(args['root'], args['target'], args['mode'], args['verbose'])
-	search.search()
-	publish_results(search.results)
-	if args['mode'] in LOOSE_COMPATIBLE_MODES:
-		choice = raw_input('Would you like to search again using the loose search method? [Y/n]')
-		if choice in POSITIVE_CHOICES:
-			from Search import LooseSearch as loose_search
-			search = loose_search(args['root'], args['target'], args['mode'], args['verbose'])
-			search.search()
-			publish_results(search.results)
+    if args['mode'] == 'ext':
+        from Search import ExtSearch as ext_search
+        print(INFO_STOP)
+        search = ext_search(args['root'], args['target'], args['verbose'])
+    else:
+        if not args['loose']:
+            from Search import Search as reg_search
+            search = reg_search(args['root'], args['target'], args['mode'], args['verbose'])
+        else:
+            from Search import LooseSearch as loose_search
+            search = loose_search(args['root'], args['target'], args['mode'], args['verbose'])
+            
+    search.search()
+    publish_results(search.results)
+    if args['mode'] in LOOSE_COMPATIBLE_MODES and not args['loose']:
+        choice = raw_input('Would you like to search again using the loose search method? [Y/n]')
+        if choice in POSITIVE_CHOICES:
+            from Search import LooseSearch as loose_search
+            search = loose_search(args['root'], args['target'], args['mode'], args['verbose'])
+            search.search()
+            publish_results(search.results)
 
 if __name__ == '__main__':
-	parser = Parser(argv[1:])
-	args = parser.parse()
-	print(args)
-	main(args)
+    parser = Parser(argv[1:])
+    args = parser.parse()
+    print(args)
+    main(args)
