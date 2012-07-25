1cc5f4f72a2ec9d453f7fd5129fd826bdb8cd1f1
Isaac Grant <waterwolf2.0@gmail.com>
2012-07-24 22:01:32 -0600

    added secondary option to do a loose search

--- ser.py
+++ ser.py
@@ -5,6 +5,9 @@ from sys import argv
 
 INFO_STOP = 'Press ctrl+c to stop the search at any time'
 SEARCH_COMPLETE = 'Search complete! Showing {0:d} results:'
+LOOSE_COMPATIBLE_MODES = ['any', 'file', 'folder']
+POSITIVE_CHOICES = ['yes', 'y', 'Y']
+NEGATIVE_CHOICES = ['no', 'n', 'N']
 
 def publish_results(results):
 	print(SEARCH_COMPLETE.format(len(results)))
@@ -21,6 +24,13 @@ def main(args):
 		search = reg_search(args['root'], args['target'], args['mode'], args['verbose'])
 	search.search()
 	publish_results(search.results)
+	if args['mode'] in LOOSE_COMPATIBLE_MODES:
+		choice = raw_input('Would you like to search again using the loose search method? [Y/n]')
+		if choice in POSITIVE_CHOICES:
+			from Search import LooseSearch as loose_search
+			search = loose_search(args['root'], args['target'], args['mode'], args['verbose'])
+			search.search()
+			publish_results(search.results)
 
 if __name__ == '__main__':
 	parser = Parser(argv[1:])
